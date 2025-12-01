3897e68fd4cd17be90ec3cff5d927ae722fb5a40
8dzw4k4jsm-collab <8dzw4k4jsm@privaterelay.appleid.com>
2025-12-01 11:10:41 -0500

    Project Setup Done

---
+++ app/agent/page.tsx
@@ -0,0 +1,9 @@
+const AgentPage = () => {
+    return (
+        <div>
+            <h1 className="text-2xl text-center">Super Agent (Aurora)</h1>
+        </div>
+    )
+}
+
+export default AgentPage;
---
+++ app/bots/page.tsx
@@ -0,0 +1,9 @@
+const BotsPage = () => {
+    return (
+        <div>
+            <h1 className="text-2xl text-center">Bots & Agents</h1>
+        </div>
+    )
+}
+
+export default BotsPage
